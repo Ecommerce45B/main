72738a3676af69181cc583c6d28d79206dfa12ac
Santiago Romero <KSantiagoRomeroO10@outlook.com>
2024-02-20 16:10:36 -0500

    Arreglando el landing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,13 +5,15 @@ import { Routes, Route } from "react-router-dom";
 // import viteLogo from '/vite.svg'
 import './App.css'
 
-import Landing from './Components/common/Landing/Landing'
+import Landing from './Components/Common/Landing/Landing'
+import Home from "./Components/Common/Home/Home";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<Landing/>}></Route>
+        <Route path='/Home' element={<Home/>}></Route>
       </Routes>
     </>
   ) 
